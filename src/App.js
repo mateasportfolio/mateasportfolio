a8c511e9c2d092eec15b960bde2bde873bfc7af2
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-05-26 17:03:02 +0200

    Add styles for landing page

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ReactComponent as InstagramIcon } from "./assets/instagram_svg.svg";
+import { ReactComponent as EpekworksIcon } from "./assets/epeklogo.svg";
+import { ReactComponent as LinkedinIcon } from "./assets/linkedin.svg";
+
+console.log();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Matea 
-        </p>
-        <p>
-          Learning reactjs.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="main-box">
+        <div className="info">
+          <h1>Welcome</h1>
+          <p>
+            I’m<b> Matea</b>
+          </p>
+          <p>
+            I'm a future Mediadesigner / Webdesigner, having an Internship at
+            <b> Epekworks.com.</b>
+          </p>
+          <p>
+            Im a <b>Free Spirit</b> ,interested into <b>Art</b>,learning{" "}
+            <b>new Skills and Technology</b>
+          </p>
+        </div>
+
+        <div className="icons">
+          <InstagramIcon />
+          <EpekworksIcon />
+          <LinkedinIcon />
+        </div>
+      </div>
+
+      <div className="second-box">
+        <div className="link">
+          <h3>FreeCodeCamp</h3>
+          <a href="url">https://www.freecodecamp.org/matea</a>
+        </div>
+        <div className="link">
+          <h3>LinkedIN</h3>
+          <a href="url">https://www.linkedin.com/in/matea-tabak-83440a230/</a>
+        </div>
+        <div className="link">
+          <h3>Netlify</h3>
+          <a href="url">https://dulcet-tiramisu-8b2b08.netlify.app/</a>
+        </div>
+      </div>
     </div>
   );
 }
