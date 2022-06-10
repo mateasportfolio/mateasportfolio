a31d0f7f44b4b39bcb0687ed35b941f2eb598faa
mateasportfolio <104080590+mateasportfolio@users.noreply.github.com>
2022-06-10 08:25:28 +0200

    changed diagrammm2

--- src/pages/Hosting/Hosting.js
+++ src/pages/Hosting/Hosting.js
@@ -1,9 +1,19 @@
 import React from "react";
 import "./style.css";
 import { data } from "./data";
-import { Section } from "../../components/Section/Section";
+import diagrammm2 from "../../assets/diagrammm2.svg";
 
 export const HostingPage = () => {
   const list = data.map((item) => <Section item={item} key={item.id} />);
-  return <div className="hosting-page">{list}</div>;
+  return (
+    <div className="hosting-page">
+      {list}
+
+      <div className="second-img">
+        <img src={diagrammm2} className="second-img" alt="second-img" />
+        <br /> <b>Diagramm</b>
+      </div>
+    </div>
+  );
 };
+export default HostingPage;
